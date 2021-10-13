8336a11f362c82ba553bd0f78539228d117e15b4
Jonas <jonasmooney2@gmail.com>
2021-10-12 20:32:30 -0600

    assignment commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('backgroundColor', '#145');
+$('li:even').css('backgroundColor', '#ccc');
+$('li:odd').css('backgroundColor', '#aaa');
+let input = $('#myInput').val('Jonas');
+console.log(input);
